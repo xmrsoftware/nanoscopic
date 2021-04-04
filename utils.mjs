dd315bde2619aa95a1bd9750eb1fa59841529a28
Simon Connah <simon.n.connah@protonmail.com>
2021-04-04 08:44:10 +0100

    Fixed only allow logged in users to see a page.

--- utils.mjs
+++ utils.mjs
@@ -17,7 +17,7 @@
 */
 
 function check_if_logged_in(req, res) {
-    if (req.session === null) {
+    if (req.session === null || req.session === undefined || req.session.logged_in !== true) {
         res.status(403).send('You are not authorised to see this URL')
     }
 }
